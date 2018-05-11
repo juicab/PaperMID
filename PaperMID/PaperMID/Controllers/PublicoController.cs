d30d1aca2b977281a2cfdcd63e5a5eb4fd03c10d
juicab <32874265+juicab@users.noreply.github.com>
2018-05-11 14:39:43 -0500

    creacion proyecto

---
+++ PaperMID/PaperMID/Controllers/PublicoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaperMID.Controllers
+{
+    public class PublicoController : Controller
+    {
+        // GET: Publico
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult QuiénesSomos()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+        public ActionResult IniciarSesión()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ PaperMID/PaperMID/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaperMID.MvcApplication" Language="C#" %>
